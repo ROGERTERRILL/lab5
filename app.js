2e4fc9591b6066ee821d002e2f967c5a7cae6249
RogerDodger <lpaddikt@gmail.com>
2019-11-14 20:40:42 -0800

    Video 3

--- app.js
+++ app.js
@@ -20,14 +20,33 @@ app.get("/", function(req, res) {
       let parsedData = JSON.parse(body);
       let imageURL = parsedData.urls.regular;
       res.render("index", { imageURL: imageURL });
+    } else {
+      res.render("index", { error: "unable to access API" });
     }
-    res.render("index", { error: "unable to access API" });
   });
 });
 
-app.get("/mercury", function(req, res) {
-  res.render("mercury.html");
-});
+app.get("/search", function(req, res) {
+  //console.dir(req);
+  // console.log(req.query.keyword);
+  let keyword = req.query.keyword;
+  let url = `https://api.unsplash.com/photos/random?query=${keyword}&count=9&client_id=0a82899cb0939e4b3b34d2e48fd5efe94fdf18c54f2970941db6873f5280d45a&orientation=landscape`;
+  request(url, function(error, response, body) {
+    // console.log("error", error);
+    // console.log("statusCode", response && response.statusCode);
+    // console.log("body", body);
+    if (!error) {
+      let parsedData = JSON.parse(body);
+      let imageURLs = [];
+      for (let i = 0; i < 9; i++) {
+        imageURLs.push(parsedData[i].urls.regular);
+      }
+      res.render("results", { imageURLs: imageURLs });
+    } else {
+      res.render("results", { error: "unable to access API" });
+    }
+  });
+}); //search
 
 app.get("/venus", function(req, res) {
   res.render("venus.html");
